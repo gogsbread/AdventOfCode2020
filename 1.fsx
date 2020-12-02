7fb1336c30159df49b7e36234f95605eba810c66
Antony Deepak Thomas <antonydeepak@gmail.com>
2020-12-02 01:09:30 -0800

    First day

--- 1.fsx
+++ 1.fsx
@@ -2,8 +2,38 @@
 
 module part1 =
     let solve input =
-        input 
-        |> Seq.iter (fun x -> printfn "%A" x)
+        input
+        |> Seq.fold (fun (product, elements: int Set) x ->
+            if elements.Contains(2020 - x) then (product * x * (2020 - x), elements) else (product, elements.Add x))
+               (1, Set.empty)
+        |> fst
 
-Common.readFromConsole
+module part2 =
+    let solve input =
+        let a = 
+            input
+            |> Seq.toArray
+
+        let n = a.Length
+        seq {
+            for i in 0 .. (n - 1) do
+                for j in (i + 1) .. (n - 1) do
+                    for k in (j + 1) .. (n - 1) -> 
+                        (a.[i], a.[j], a.[k])
+        }
+        |> Seq.pick (fun (x, y, z) ->
+            match (x + y + z) with
+            | 2020 -> Some (x * y * z)
+            | _ -> None)
+
+let input = Common.readIn
+
+input
+|> Seq.map int
 |> part1.solve
+|> Common.writeOut
+
+input
+|> Seq.map int
+|> part2.solve
+|> Common.writeOut
--- common.fs
+++ common.fs
@@ -2,6 +2,10 @@ module Common
 
     open System
 
-    let readFromConsole = 
+    let readIn = 
         Seq.initInfinite (fun _ -> Console.ReadLine())
         |> Seq.takeWhile (fun line -> (not (isNull line)))
+        |> Seq.toList
+
+    let writeOut output =
+        printfn "%A" output
