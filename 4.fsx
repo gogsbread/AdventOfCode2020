dcf432cd3a8ff8c91f01e9097a722c3b84ed9e14
Antony Deepak Thomas <antonydeepak@gmail.com>
2020-12-04 23:24:12 -0800

    Some ideas for 4

---
+++ 4.fsx
@@ -0,0 +1,4 @@
+// Create two states -> passport field and no passport field
+// Write an active pattern that gives passportfield if string <> empty else None
+// Do active pattern matching with 1,2..8 as list recursively; generate a list of records that way
+// just pure logic of asserting that all 7 other fields are present
