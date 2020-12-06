aeab0e5dd8f2cf3ec2fd85e42193e901dff731ad
Antony Deepak Thomas <antonydeepak@gmail.com>
2020-12-05 23:29:24 -0800

    Format day 5

--- 5.fsx
+++ 5.fsx
@@ -15,18 +15,17 @@ let rec id s e (space: Part list) =
 
 let toSection (input: string) =
     input.ToCharArray()
-    |> Seq.map(fun x -> 
+    |> Seq.map (fun x ->
         match x with
         | 'F' -> U
         | 'B' -> L
         | 'L' -> U
         | 'R' -> L
-        | _ -> raise (InvalidOperationException("Input invalid"))
-    )
+        | _ -> raise (InvalidOperationException("Input invalid")))
 
-let sections (s: string) = 
-    let r = s.[0 .. 6]
-    let c = s.[7 ..]
+let sections (s: string) =
+    let r = s.[0..6]
+    let c = s.[7..]
     (toSection r, toSection c)
 
 let seatId (row: seq<Part>, col: seq<Part>) =
@@ -40,30 +39,26 @@ let seatId (row: seq<Part>, col: seq<Part>) =
 
 module part1 =
     let solve (input: string list) =
-        input
-        |> List.map (sections >> seatId)
-        |> List.max
+        input |> List.map (sections >> seatId) |> List.max
 
-module part2 = 
+module part2 =
     let solve (input: string list) =
         let rec missingSeats (expected: int) (sortedSeats: int list) =
             match sortedSeats with
             | [] -> []
-            | actual::rest ->
-                if expected <> actual then
-                    expected::(missingSeats (actual + 1) rest)
-                else
-                    missingSeats (expected + 1) rest
+            | actual :: rest ->
+                if expected <> actual
+                then expected :: (missingSeats (actual + 1) rest)
+                else missingSeats (expected + 1) rest
 
         let seats =
             input
             |> List.map (sections >> seatId)
             |> List.sort
+
         (missingSeats seats.Head seats).Head
 
 
 let input = Common.readIn
 input |> part1.solve |> Common.writeOut
 input |> part2.solve |> Common.writeOut
-// input |> List.map part1.sections |> List.map part1. seatId |> Common.writeOut
-// fst input |> toSection |> Seq.toList |> id 0 
