6ed3722a2c1830c33362c10760c4425ba846385d
Antony Deepak Thomas <antonydeepak@gmail.com>
2020-12-08 00:16:44 -0800

    7 part 1

---
+++ 7.fsx
@@ -0,0 +1,51 @@
+#load "common.fs"
+
+open System.Text.RegularExpressions
+
+type Content = { Name: string; Count: int }
+
+let parse (input: string) =
+    let container =
+        Regex.Match(input, @"(\w+ \w+) bags contain").Groups.[1].Value
+
+    let contents =
+        Regex.Matches(input, @"(\d+) (\w+ \w+) bag(s)?")
+        |> Seq.map (fun m ->
+            { Name = m.Groups.[2].Value
+              Count = int (m.Groups.[1].Value) })
+        |> Seq.toList
+
+    (container, contents)
+
+module part1 =
+    let rec containersOf (bag: string) (containerMappings: Map<string, List<string>>) (knownContainers: Set<string>) =
+        match containerMappings.ContainsKey(bag) with
+        | true ->
+            containerMappings.[bag]
+            |> List.fold (fun (acc: Set<string>) (b: string) ->
+                if not (acc.Contains(b)) then containersOf b containerMappings (acc.Add b) else acc) knownContainers
+        | false -> knownContainers
+
+    let solve (input: List<string>) =
+        let rec vToU u v initialState =
+            match v with
+            | [] -> initialState
+            | v1 :: rest ->
+                let m =
+                    Map.change v1.Name (fun t ->
+                        match t with
+                        | Some x -> Some(x @ [ u ])
+                        | None -> Some [ u ]) initialState
+
+                vToU u rest m
+
+        let containerMappings =
+            input
+            |> List.map parse
+            |> List.fold (fun acc (u, v) -> vToU u v acc) Map.empty
+
+        containersOf "shiny gold" containerMappings Set.empty
+        |> Set.count
+
+let input = Common.readIn
+input |> part1.solve |> Common.writeOut
