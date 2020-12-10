82438c929867907f55ea5857d75ad7c188209d6e
Antony Deepak Thomas <antonydeepak@gmail.com>
2020-12-09 23:03:18 -0800

    Simplify day one

--- 1.fsx
+++ 1.fsx
@@ -1,36 +1,20 @@
 #load "common.fs"
 
 module part1 =
-    let solve input =
-        input
-        |> Seq.fold (fun (product, elements: int Set) x ->
-            if elements.Contains(2020 - x) then (product * x * (2020 - x), elements) else (product, elements.Add x))
-               (1, Set.empty)
-        |> fst
+    let solve (input: List<string>) =
+        let expenses = input |> List.map int
+        List.allPairs expenses expenses
+        |> List.pick (fun (x, y) -> if x + y = 2020 then Some(x * y) else None)
 
 module part2 =
     let solve input =
-        let a = input |> Seq.toArray
-
-        let n = a.Length
-        seq {
-            for i in 0 .. (n - 1) do
-                for j in (i + 1) .. (n - 1) do
-                    for k in (j + 1) .. (n - 1) -> (a.[i], a.[j], a.[k])
-        }
-        |> Seq.pick (fun (x, y, z) ->
-            match (x + y + z) with
-            | 2020 -> Some(x * y * z)
-            | _ -> None)
+        let expenses = input |> List.map int
+        let pairs = List.allPairs expenses expenses
+        List.pick (fun (x, y, z) -> if x + y + z = 2020 then Some(x * y * z) else None)
+            [ for x in expenses do
+                for (y, z) in pairs do
+                    (x, y, z) ]
 
 let input = Common.readIn
-
-input
-|> Seq.map int
-|> part1.solve
-|> Common.writeOut
-
-input
-|> Seq.map int
-|> part2.solve
-|> Common.writeOut
+input |> part1.solve |> Common.writeOut
+input |> part2.solve |> Common.writeOut
