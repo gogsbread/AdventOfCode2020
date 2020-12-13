4456d08cd3af31700ef923c6cc9a7760ce6f25eb
Antony Deepak Thomas <antonydeepak@gmail.com>
2020-12-12 22:27:26 -0800

    Day 10

--- 10.fsx
+++ 10.fsx
@@ -1,39 +1,38 @@
 #load "common.fs"
 
+open System.Collections.Generic
+
 module part1 =
-    let solve (input: List<string>) =
-            let diffs =
-                input
-                |> List.map int
-                |> List.sort
-                |> List.mapFold (fun acc j -> (j - acc), j) 0
-                |> fst
-                |> List.countBy id
-                |> Map.ofList
-            diffs.[1] * (diffs.[3] + 1)
-
-module part2 =
-    let rec arrangements (adapters: List<int>) =
-        match adapters with
-        | f::s::t::fr::_ -> 
-            if fr - f = 3 then
-                (1 <<< 2) * arrangements (List.skip 3 adapters)
-            elif t - f = 2 then
-                (1 <<< 1) * arrangements (List.skip 2 adapters)
-            else
-                arrangements (List.skip 1 adapters)
-        | f::s::t::_ -> 
-            if t - f <= 3 then 2 else 1
-        | _ ->
-            1
-
-    let solve (input: List<string>) =
-        let x = 
+    let solve (input: string list) =
+        let diffs =
             input
             |> List.map int
             |> List.sort
-        let adapters = [0] @ x @ [(List.max x) + 3]
-        arrangements adapters
+            |> List.mapFold (fun acc j -> (j - acc), j) 0
+            |> fst
+            |> List.countBy id
+            |> Map.ofList
+
+        diffs.[1] * (diffs.[3] + 1)
+
+module part2 =
+    let solve (input: string list) =
+        let x = input |> List.map int |> List.sort
+        let adapters = [ 0 ] @ x @ [ List.max x + 3 ]
+
+        let dp = Dictionary<int, int64>([ KeyValuePair(adapters.[0], 1L) ])
+
+        for i in [ 1 .. adapters.Length - 1 ] do
+            let v =
+                List.sum [ for j in [ 1 .. 3 ] ->
+                               if (i - j >= 0)
+                                  && (adapters.[i] - adapters.[i - j] <= 3) then
+                                   dp.[adapters.[i - j]]
+                               else
+                                   0L ]
+
+            dp.Add(adapters.[i], v)
+        dp.[adapters |> List.last]
 
 let input = Common.readIn
 input |> part1.solve |> Common.writeOut
