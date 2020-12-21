6e10cf6c41268ef4cfd450c230f32e7c3d75b07d
Antony Deepak Thomas <antonydeepak@gmail.com>
2020-12-21 09:10:51 -0800

    Day 18 scratch

---
+++ 18.fsx
@@ -0,0 +1,3 @@
+// return partially applied operators when you encounter symbols
+// try using reduce
+// if you get a symbol partially apply that symbol to the first argument and return the value as accumulator
