5d000745969c9695b6bf45f2b51db923546cf117
Antony Deepak Thomas <antonydeepak@gmail.com>
2020-12-21 16:03:22 -0800

    Some options for day 18

--- 18.fsx
+++ 18.fsx
@@ -1,3 +1,45 @@
-// return partially applied operators when you encounter symbols
-// try using reduce
-// if you get a symbol partially apply that symbol to the first argument and return the value as accumulator
+// add symbol to it
+
+#load "common.fs"
+
+type Symbol =
+    | Add
+    | Mul
+    | OpenBrace
+    | CloseBrace
+
+type Operand = int
+
+type Token =
+    | Symbol of Symbol
+    | Operand of Operand
+
+let toToken (c: char): Token =
+    match c with
+    | '+' -> Symbol Add
+    | '*' -> Symbol Mul
+    | '(' -> Symbol OpenBrace
+    | ')' -> Symbol CloseBrace
+    | _ -> Operand (int (c.ToString()))
+
+module part1 =
+    let toPostfix (tokens: list<Token>): list<Token> =
+        let operands, operators =
+            tokens
+            |> List.fold (fun (operands: list<Operand>, operators: list<Symbol>) (token: Token) ->
+                    match token with
+                    | Operand t -> t::operands, operators
+                    | Symbol s -> operands, operators@[s]
+                ) (List.empty, List.empty)
+        (operands |> List.map Operand) @ (operators |> List.map Symbol)
+
+    let solve (input: list<string>) =
+        let expr = input.Head
+        expr
+        |> Seq.filter (fun c -> c <> ' ')
+        |> Seq.map toToken
+        |> Seq.toList
+        |> toPostfix
+
+let input = Common.readIn
+input |> part1.solve |> Common.writeOut
