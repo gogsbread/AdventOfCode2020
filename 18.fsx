95f4b11d1e0e9dad8f3723ab91672593c5472e49
Antony Deepak Thomas <antonydeepak@gmail.com>
2020-12-22 09:22:34 -0800

    Some more progress on day 18

--- 18.fsx
+++ 18.fsx
@@ -26,12 +26,23 @@ module part1 =
     let toPostfix (tokens: list<Token>): list<Token> =
         let operands, operators =
             tokens
-            |> List.fold (fun (operands: list<Operand>, operators: list<Symbol>) (token: Token) ->
+            |> List.fold (fun (tokens: list<Token>, operators: list<Token>) (token: Token) ->
                     match token with
-                    | Operand t -> t::operands, operators
-                    | Symbol s -> operands, operators@[s]
+                    | Operand t -> tokens @ [Operand t], operators
+                    | Symbol s -> 
+                        match s with
+                        | OpenBrace -> tokens, Symbol s::operators
+                        | CloseBrace -> 
+                            let i = List.findIndex (fun op -> op = Symbol OpenBrace) operators
+                            // printfn "%A %A" i operators
+                            tokens @ (operators.[ .. i - 1]), operators.[ i + 1 .. ]
+                        | _ -> 
+                            let iOpenBrace = List.tryFindIndex (fun op -> op = Symbol OpenBrace) operators
+                            match iOpenBrace with
+                            | Some i -> tokens @ (operators.[ .. i - 1]), Symbol s::operators.[i .. ]
+                            | None -> tokens @ operators, [Symbol s]
                 ) (List.empty, List.empty)
-        (operands |> List.map Operand) @ (operators |> List.map Symbol)
+        operands @ operators
 
     let solve (input: list<string>) =
         let expr = input.Head
