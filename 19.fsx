f521dc489e6638ad08bfecee8e4c74b9486c8d4d
Antony Deepak Thomas <antonydeepak@gmail.com>
2020-12-23 15:33:40 -0800

    Day 19

--- 19.fsx
+++ 19.fsx
@@ -15,8 +15,10 @@ type Rule =
 
 let parseRule (s: string): option<(SubRule * Rule)> =
     let subrulesRe = Regex.Match(s, @"(\d+): (\d+(\s\d+)*)")
+
     let choiceSubrulesRe =
         Regex.Match(s, @"(\d+): (\d+(\s\d+)*)\s\|\s(\d+(\s\d+)*)")
+
     let terminalRe = Regex.Match(s, @"(\d+): ""(\w)""")
 
     if choiceSubrulesRe.Success then
@@ -32,7 +34,7 @@ let parseRule (s: string): option<(SubRule * Rule)> =
             |> Array.map int
             |> Array.toList
 
-        Some (n, ChoiceSubRules(sr1, sr2))
+        Some(n, ChoiceSubRules(sr1, sr2))
     elif subrulesRe.Success then
         let n = int subrulesRe.Groups.[1].Value
 
@@ -41,19 +43,16 @@ let parseRule (s: string): option<(SubRule * Rule)> =
             |> Array.map int
             |> Array.toList
 
-        Some (n, SubRules sr)
+        Some(n, SubRules sr)
     elif terminalRe.Success then
         let n = int terminalRe.Groups.[1].Value
-        Some (n, Terminal terminalRe.Groups.[2].Value)
+        Some(n, Terminal terminalRe.Groups.[2].Value)
     else
         None
 
-let parseMessage(s: string):  option<string> =
-    let messageRe = Regex.Match(s,  @"^[a-b]+$")
-    if messageRe.Success then
-        Some messageRe.Value
-    else
-        None
+let parseMessage (s: string): option<string> =
+    let messageRe = Regex.Match(s, @"^[a-b]+$")
+    if messageRe.Success then Some messageRe.Value else None
 
 let cache = Dictionary<SubRule, list<Terminal>>()
 
@@ -71,7 +70,9 @@ let rec eval (r: Rule) (rules: Map<SubRule, Rule>): list<Terminal> =
     | SubRules (h :: rest) ->
         List.allPairs (eval (SubRules [ h ]) rules) (eval (SubRules rest) rules)
         |> List.map (fun (a, b) -> a + b)
-    | ChoiceSubRules (sr1, sr2) -> (eval (SubRules sr1) rules) @ (eval (SubRules sr2) rules)
+    | ChoiceSubRules (sr1, sr2) ->
+        (eval (SubRules sr1) rules)
+        @ (eval (SubRules sr2) rules)
 
 module part1 =
     let solve (input: list<string>) =
@@ -80,28 +81,24 @@ module part1 =
             |> List.choose parseRule
             |> List.fold (fun acc (n, r) -> Map.add n r acc) Map.empty
 
-        let messages =
-            input
-            |> List.choose parseMessage
+        let messages = input |> List.choose parseMessage
 
-        let validMessages = Set (eval rules.[0] rules)
+        let validMessages = Set(eval rules.[0] rules)
 
-        (messages
-        |> List.filter validMessages.Contains).Length
+        (messages |> List.filter validMessages.Contains)
+            .Length
+
 
 module part2 =
-    // This is a lil wonky solution and suspect that it may not work for some inputs
     let invalid (input: list<string>) =
         let rules =
             input
             |> List.choose parseRule
             |> List.fold (fun acc (n, r) -> Map.add n r acc) Map.empty
 
-        let messages =
-            input
-            |> List.choose parseMessage
+        let messages = input |> List.choose parseMessage
 
-        let validMessages = Set (eval rules.[0] rules)
+        let validMessages = Set(eval rules.[0] rules)
 
         messages
         |> List.filter (validMessages.Contains >> not)
@@ -112,32 +109,38 @@ module part2 =
             |> List.choose parseRule
             |> List.fold (fun acc (n, r) -> Map.add n r acc) Map.empty
 
-        let messages =
-            input
-            |> List.choose parseMessage
+        let messages = input |> List.choose parseMessage
 
         let fortyTwoMessages = eval rules.[42] rules
         let thirtyOneMessages = eval rules.[31] rules
         let invalidMessages = invalid input
-        let rec prefixMatch (s: string) (l: list<Terminal>) =
+
+        let rec prefixMatch (s: string) (count: int) (l: list<Terminal>) =
             match List.tryFind (fun (ls: string) -> s.StartsWith(ls)) l with
-            | Some m -> prefixMatch s.[m.Length .. ] l
-            | None -> s
+            | Some m -> prefixMatch s.[m.Length..] (count + 1) l
+            | None -> (count, s)
 
         let additional =
             (invalidMessages
-            |> List.choose
-                (fun m ->
-                    let remaining = prefixMatch m fortyTwoMessages
-                    if String.IsNullOrEmpty(remaining) then
-                        None
-                    else
-                        let rest = prefixMatch remaining thirtyOneMessages
-                        if String.IsNullOrEmpty(rest) then
-                            Some m
-                        else
-                            None
-                )).Length
+             |> List.choose
+                 (fun m ->
+                     let (countFortyTwo, remaining) = prefixMatch m 0 fortyTwoMessages
+
+                     if countFortyTwo < 2
+                        || String.IsNullOrEmpty(remaining) then
+                         None
+                     else
+                         let (countThirtyOne, rest) =
+                             prefixMatch remaining 0 thirtyOneMessages
+
+                         if countThirtyOne >= 1
+                            && countFortyTwo > countThirtyOne
+                            && String.IsNullOrEmpty(rest) then
+                             Some m
+                         else
+                             None))
+                .Length
+
         part1.solve input + additional
 
 let input = Common.readIn
